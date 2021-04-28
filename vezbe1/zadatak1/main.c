2eddd148d62f401cd4812d84b8cbe888a441bdc1
jelenakaran1 <jelenakran99@gmail.com>
2021-04-28 10:26:36 +0200

    Promena

--- vezbe1/zadatak1/main.c
+++ vezbe1/zadatak1/main.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
+
+int Calculation(int a, int b)
+{
+if(a > b)
+return a - b;
+else
+return b - a;
+}
+
 int main()
 {
 int a = 5;
 int b = 4;
 int c;
-if(a > b)
-c = a - b;
-else
-c = b - a;
+
+c = Calculation(a,b);
+//promena Jelena
+
 printf("Result: %d\n", c);
+
 return 0;
 }
