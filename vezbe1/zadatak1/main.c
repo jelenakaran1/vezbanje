cc16c26e3c14d3fe6d0fe1c0b10ffdc9a19a2fa2
jelenakaran1 <jelenakran99@gmail.com>
2021-04-28 09:53:41 +0200

    Caculation function added

--- vezbe1/zadatak1/main.c
+++ vezbe1/zadatak1/main.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+
+int Calculation(int a, int b)
+{
+if(a > b)
+return a - b;
+else
+return b - a;
+}
+
 int main()
 {
 int a = 5;
 int b = 4;
 int c;
-if(a > b)
-c = a - b;
-else
-c = b - a;
+
+c = Calculation(a,b);
+
 printf("Result: %d\n", c);
+
 return 0;
 }
